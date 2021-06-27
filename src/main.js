ee2bfc7ca7798276d8fc9222d57f3a0887032189
mjungp <mjungpp@naver.com>
2021-06-28 00:16:07 +0900

    JS : MBTI 문제 및 JSON Data 불러오기 구현중

--- src/main.js
+++ src/main.js
@@ -2,11 +2,52 @@
 
 const startBtn = document.querySelector('.mbti__startBtn');
 const startContent = document.querySelector('.mbti__start');
+const title = document.querySelector('.mbti__asking');
 const questionContent = document.querySelector('.mbti__question.--hide');
-console.log(questionContent);
+const progressBar = document.querySelector('.skill__value');
+const selectBtn = document.querySelector('.mbti__selectBtn');
+const type = document.querySelector('#type').dataset.value;
+const values = document.querySelectorAll('input');
+let questionNum = 1;
 
-startBtn.addEventListener('click', () => {
+startBtn.addEventListener('click', startTest);
+
+function startTest(){
     startContent.classList.add('--hide');
     questionContent.classList.remove('--hide');
+    proceedProgressBar();
+    selectBtn.addEventListener('click',countScore);
+}
+
+// progress bar를 진행 상황에 따라 증가
+function proceedProgressBar() {
+
+}
+
+function questionTitle() {
+    title.innerHTML = `${}`;
+}
+
+function countScore() {
+    console.log('initScore 실행');
+    console.log(type);
+    values.forEach(value => {
+        if(type == value.dataset.type){
+            console.log(++value.dataset.value);
+        }
+    });
+}
+
+loadQuestions();
+
+function loadQuestions() {
+    return fetch('../data/data.json')
+    .then(response => response.json())
+    .then(json =>json.items);
+}
+
+loadQuestions()
+.then(items => {
+    console.log(items);
 });
 
